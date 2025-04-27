7f3544fac959b5866736051851143ce5bd3fdfcb
Kholik Nesonov <91988209+Nesaq@users.noreply.github.com>
2025-04-28 01:03:35 +0300

    first iteration of render data

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-import { useFetchData } from '@/composable/useContents'
-import { onMounted, watch } from 'vue'
+import { onMounted } from 'vue'
+import { useFetchData } from '../composable/useContents'
+import TreeNode from './TreeNode.vue'
 
-const { contents, loading, error, fetchContents } = useFetchData()
+const { loading, error, fetchContents, tree } = useFetchData()
 
 defineProps<{
   msg: string
@@ -11,26 +12,32 @@ defineProps<{
 onMounted(() => {
   fetchContents()
 })
-
-watch(contents, (val) => {
-  if (val) {
-    console.log('Оглавление загружено:', val)
-  }
-})
 </script>
 
 <template>
-  <div>
+  <div class="table-of-contents">
     <h1>Оглавление</h1>
     <div v-if="loading">Загрузка...</div>
     <div v-else-if="error">Ошибка: {{ error.message }}</div>
-    <div v-else>data загружены</div>
-    {{ msg }}
+    <ul v-else-if="tree.length > 0">
+      <TreeNode v-for="rootNode in tree" :key="rootNode.key" :node="rootNode" />
+    </ul>
+    <div v-else>Нет данных</div>
   </div>
 </template>
 
 <style scoped>
-h1 {
-  color: red;
+.table-of-contents {
+  max-width: 300px;
+  padding: 1rem;
+  border-right: 1px solid #eee;
+  font-family: sans-serif;
+  height: 100vh;
+  overflow-y: auto;
+}
+
+ul {
+  list-style-type: none;
+  padding-left: 0px;
 }
 </style>
---
+++ src/components/TreeNode.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import type { TreeNode } from '../types/ApiType'
+
+defineProps<{
+  node: TreeNode
+}>()
+</script>
+
+<template>
+  <li>
+    <span>{{ node.name }}</span>
+    <ul v-if="node.children && node.children.length > 0">
+      <TreeNode v-for="child in node.children" :key="child.key" :node="child" />
+    </ul>
+  </li>
+</template>
+
+<style scoped>
+ul {
+  margin-left: 20px;
+  /* list-style-type: none; */
+  padding-left: 0;
+}
+li {
+  margin-top: 5px;
+}
+</style>
