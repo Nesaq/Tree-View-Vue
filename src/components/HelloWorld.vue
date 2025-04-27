bc404f047fd72ab89be8f8e65c901a4a57e06be8
Kholik Nesonov <91988209+Nesaq@users.noreply.github.com>
2025-04-27 15:28:00 +0300

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div>
+    <h1>Hello World</h1>
+    <h2>Hello World</h2>
+    {{ msg }}
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
